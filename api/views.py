3f23e57f557a95f74ae3c7b6878a3ec6805717cd
Maddie Eletva <eletva09@gmail.com>
2025-04-01 21:35:39 +1100

    Requests are now post only

--- api/views.py
+++ api/views.py
@@ -2,74 +2,91 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import HttpRequest
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 from api.models.user import User
 from api.models.game_state import GameState
 
+# @csrf_exempt BAD TO DO but for now we ball
+@csrf_exempt
 # Currently makes a new user and passes the ID and name back
 def login(request : HttpRequest):
+    if(request.method == 'POST'):   
 
-    last_user_id = User.objects.count() + 1
-    new_username = f"User-{last_user_id}"
-    user = User.objects.create(user_name = new_username)
-    user.save()
+        last_user_id = User.objects.count() + 1
+        new_username = f"User-{last_user_id}"
+        user = User.objects.create(user_name = new_username)
+        user.save()
 
-    response_body = {
-        "id": user.id,
-        "user_name": user.user_name
-    }
-    
-    return HttpResponse(json.dumps(response_body), content_type="application/json")
+        response_body = {
+            "id": user.id,
+            "user_name": user.user_name
+        }
+        
+        return JsonResponse(response_body)
+    else:
+        return JsonResponse({
+            "error": True,
+            "message": "Stop It"
+        }, status=403)
 
-# 
+# @csrf_exempt BAD TO DO but for now we ball
+@csrf_exempt
 def upload(request : HttpRequest):
 
-    request_okay = _verify_upload(request)
-    # See below as the interface of return body
-    return_body = {
-        "error": False,
-        "message": "",
-        "code": "",
-        "enemy_user": "",
-        "enemy_game_state": ""
-    }
-    status = 200
-    if(request_okay):
-        body_json = json.loads(request.body)
-        # Check for User
-        USER_LIST : User = User.objects.all().filter(id = body_json["user"])
-        if(len(USER_LIST) > 0):
-            USER = USER_LIST[0]
-            PotentialGameStates = GameState.objects.all().exclude(user = USER).filter(turn = body_json["turn"])
-            # For now we are just going to return the first one found  
-            if(len(PotentialGameStates) == 0):
-                return_body["error"] = True
-                return_body["code"] = "GD001"
-                return_body["message"] = "ERROR - No Available Enemy Data"
-                status = 400
+    if(request.method == 'POST'):
+        # See below as the interface of return body
+        return_body = {
+            "error": False,
+            "message": "",
+            "code": "",
+            "enemy_user": "",
+            "enemy_game_state": ""
+        }
+        request_okay = _verify_upload(request)
+        status = 200
+        if(request_okay):
+            body_json = json.loads(request.body)
+            # Check for User
+            USER_LIST : User = User.objects.all().filter(id = body_json["user"])
+            if(len(USER_LIST) > 0):
+                USER = USER_LIST[0]
+                PotentialGameStates = GameState.objects.all().exclude(user = USER).filter(turn = body_json["turn"])
+                # For now we are just going to return the first one found  
+                if(len(PotentialGameStates) == 0):
+                    return_body["error"] = True
+                    return_body["code"] = "GD001"
+                    return_body["message"] = "ERROR - No Available Enemy Data"
+                    status = 400
 
+                else:
+                    SelectedGameState : GameState = PotentialGameStates[0]
+                    return_body["message"] = "SUCCESS - Enemy Data Found"
+                    return_body["enemy_user"] = SelectedGameState.user.user_name
+                    return_body["enemy_game_state"] = SelectedGameState.map
+
+                    GameState(user=USER, turn=body_json["turn"], map=body_json["game_state"]).save()
             else:
-                SelectedGameState : GameState = PotentialGameStates[0]
-                return_body["message"] = "SUCCESS - Enemy Data Found"
-                return_body["enemy_user"] = SelectedGameState.user.user_name
-                return_body["enemy_game_state"] = SelectedGameState.map
-
-                GameState(user=USER, turn=body_json["turn"], map=body_json["game_state"]).save()
+                return_body["error"] = True
+                return_body["message"] = "ERROR - User Does Not Exist"
+                return_body["code"] = "US001"
+                status = 400
+                
         else:
             return_body["error"] = True
-            return_body["message"] = "ERROR - User Does Not Exist"
-            return_body["code"] = "US001"
+            return_body["message"] = "ERROR - Request Invalid"
+            return_body["code"] = "RE001"
             status = 400
-            
-    else:
-        return_body["error"] = True
-        return_body["message"] = "ERROR - Request Invalid"
-        return_body["code"] = "RE001"
-        status = 400
 
-    # Return return_body
-    return HttpResponse(json.dumps(return_body), content_type="application/json", status=status)
+        # Return return_body
+        return JsonResponse(return_body, status=status)
+    else:
+        return JsonResponse({
+            "error": True,
+            "message": "Stop It"
+        }, status=403)
     
 def _verify_upload(request : HttpRequest):
     return True
