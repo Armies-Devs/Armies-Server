bd4fb56be8515713e1f03826aa8b5db9cb264c7e
Maddie Eletva <eletva09@gmail.com>
2025-05-28 15:30:39 +1000

    is_guest_user boolean field with default true

--- api/models/user.py
+++ api/models/user.py
@@ -5,7 +5,8 @@ from django.db import models
 class User(models.Model):
     # TODO: chuck in a config file
     user_name = models.CharField(max_length=30)
+    is_guest_user = models.BooleanField(default=True)
     
     # Stringify
     def __str__(self):
-        return f"ui:{self.id} - user_name:{self.user_name}"
+        return f"ui:{self.id} - user_name:{self.user_name} - guest_user:{self.is_guest_user}"
